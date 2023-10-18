8cb6aecc23aac6d119b8efd21ad102ad7b743a24
Anum Mian <anum.mian@guardian.co.uk>
2023-10-18 17:21:28 +0100

    basic list of contacts

--- Workshop/ContentView.swift
+++ Workshop/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import LoremSwiftum
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(1..<10) { num in
+                    ZStack {
+                        HStack(spacing: 20) {
+                            Image("person\(num)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 70, height: 70)
+                                .foregroundColor(.gray)
+                                .clipShape(.circle)
+
+                            VStack(alignment: .leading) {
+                                Text(Lorem.fullName)
+                                    .fontWeight(.heavy)
+
+                                Text("0780897677\(num)")
+                            }
+
+                            Spacer()
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .padding(.horizontal, 10)
+            .navigationTitle("Contacts")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
