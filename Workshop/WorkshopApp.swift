11dbda4370397cfad6e362a239e9e43d9ca82f46
Anum Mian <anum.mian@guardian.co.uk>
2023-10-18 13:29:27 +0100

    Initial Commit

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by Anum Mian on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
